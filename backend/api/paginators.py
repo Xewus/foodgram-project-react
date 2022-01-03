2c5aca2bc4eff21f82e472220a32f4ed436496e0
Xewus <xewuss@yandex.ru>
2022-01-03 10:30:38 +0300

    dev.1.1.20 flake-quote

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -2,8 +2,8 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class PageLimitPagination(PageNumberPagination):
-    '''
+    """
     Стандартный пагинатор.
     Переименовано имя параметра под требования фронтенда.
-    '''
+    """
     page_size_query_param = 'limit'
